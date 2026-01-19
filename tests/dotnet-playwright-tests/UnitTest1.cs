1fc1d62998817f291ba073b3746ca241961d566a
X4\/¡ G <xavier.gonzalez.arriola.liza@gmail.com>
2026-01-19 11:28:33 +0100

    ci: add dotnet-playwright workflow and C# Playwright tests

---
+++ tests/dotnet-playwright-tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace dotnet_playwright_tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
